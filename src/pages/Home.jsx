a934428b86bea9d9596b7a76f8e27efd73abe7e2
Alana <halo2382001@yahoo.com>
2022-09-06 14:50:43 -0400

    set up file and started build of first page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Main from '../components/Main'
+
+const Home = () => {
+  return (
+    <div>
+        
+        <Main/>
+
+    </div>
+  )
+}
+
+export default Home
