bf89f101a3a4499e8b2bcac10d9fca58dbb354a5
Alana <halo2382001@yahoo.com>
2022-09-06 14:54:04 -0400

    finished main page

--- src/Requests.js
+++ src/Requests.js
@@ -1,5 +1,3 @@
-
-
 const requests = {
   requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
   requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,7 +13,15 @@ const Main = () => {
     });
   }, {});
 
-//   console.log(movie);
+  //   console.log(movie);
+
+  const truncateString = (str, num) => {
+    if (str?.length > num) {
+      return str.slice(0, num) + "...";
+    } else {
+      return str;
+    }
+  };
 
   return (
     <div className="w-full h-[550px] text-white">
@@ -26,7 +34,7 @@ const Main = () => {
         />
         <div className="absolute w-full top-[20%] p-4 md:p-8">
           <h1 className="text-3xl md:text-5xl font-bold">{movie?.title}</h1>
-          <div className='my-4'>
+          <div className="my-4">
             <button className="border bg-gray-300 text-black border-grey-300 py-2 px-5">
               Play
             </button>
@@ -34,8 +42,12 @@ const Main = () => {
               Watch Later
             </button>
           </div>
-          <p className='text-grey-400 text-sm'>Release: {movie?.release_date}</p>
-          <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-grey-200'>{movie?.overview}</p>
+          <p className="text-grey-400 text-sm">
+            Release: {movie?.release_date}
+          </p>
+          <p className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-grey-200">
+            {truncateString(movie?.overview, 150)}
+          </p>
         </div>
       </div>
     </div>
