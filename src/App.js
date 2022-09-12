51f969ffc4521246d828e4afb60f825257bbd4e3
Alana <halo2382001@yahoo.com>
2022-09-12 14:29:45 -0400

    working on signup page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { MdAccountBalance } from "react-icons/md";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { AuthContextProvider } from "./context/AuthContext";
 import Home from "./pages/Home";
+import { Login } from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
 
 function App() {
   return (
@@ -10,6 +14,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account />} />
         </Routes>
       </AuthContextProvider>
     </>
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <>
+      <div className="w-full h-screen">
+        <img
+          className="hidden sm:block absolute w-full h-full object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/701eec50-4b87-4dc0-9d00-b0f54025dc36/489dfafa-cdde-45c9-a615-496695e7ffab/US-en-20220905-popsignuptwoweeks-perspective_alpha_website_large.jpg "
+          alt="/"></img>
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
+        <div className="fixed w-full px-4 py-24 z-50">
+          <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+            <div className="max-w-[320px] mx-auto py-16">
+              <h1 className="text-3xl font-bold">Sign Up</h1>
+              <form className="w-full flex flex-col py-4">
+                <input
+                  className="p-3 my-2 bg-gray-700 rounded"
+                  type="email"
+                  placeholder="Email"
+                  // autoComplete="email"
+                />
+                <input
+                  className="p-3 my-2 bg-gray-700 rounded"
+                  type="password"
+                  placeholder="Password"
+                  // autoCompete="current-password"
+                />
+                <button className="bg-red-600 py-3 my-6 rounded font-bold">
+                  Sign Up
+                </button>
+                <div className="flex justify-between items-center">
+                  <p>
+                    <input className="mr-2" type="checkbox" />
+                    Remember Me
+                  </p>
+                  <p>Need Help?</p>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
